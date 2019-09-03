13342a5613e05c9072e3c56671bfe4b080a88a69
Joel Kerfoot <joelkerfoot@gmail.com>
2019-09-02 19:18:34 -0700

    cleaned up login modal, closes #25

--- src/Apps/SignIn.jsx
+++ src/Apps/SignIn.jsx
@@ -3,17 +3,25 @@ import { Button, Modal, Paper, withStyles } from '@material-ui/core';
 import React from 'react';
 import SignInForm from '../CompositeComponents/Authentication/SignInForm';
 
-const styles = {
+const styles = theme => ({
 	modal: {
-		top: `${50}%`,
-		left: `${50}%`,
-		transform: `translate(-${50}%, -${50}%)`,
+		[theme.breakpoints.up('xs')]: {
+			top: `${10}%`,
+			left: `${10}%`,
+			transform: `translate(-${5}%, -${5}%)`
+		},
+		[theme.breakpoints.up('sm')]: {
+			top: `${50}%`,
+			left: `${50}%`,
+			transform: `translate(-${50}%, -${50}%)`
+		},
 		position: 'absolute',
-		width: 500,
-		border: '2px solid #000',
+		padding: '2em',
+		maxWidth: 350,
+		border: '1px solid #000',
 		outline: 'none'
 	}
-};
+});
 
 const SignIn = ({ classes, buttonText }) => {
 	const [open, setOpen] = React.useState(false);
--- src/CompositeComponents/Authentication/SignInForm.jsx
+++ src/CompositeComponents/Authentication/SignInForm.jsx
@@ -1,9 +1,20 @@
-import { Button, Grid, TextField, Typography } from '@material-ui/core';
+import { Button, Grid, TextField, Typography, withStyles } from '@material-ui/core';
+import {
+	FacebookLoginButton,
+	GoogleLoginButton,
+	TwitterLoginButton
+} from 'react-social-login-buttons';
 import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
 import { withFirebase } from '../../Components/Firebase';
 import { withRouter } from 'react-router-dom';
+
+const styles = {
+	signUp: {
+		paddingTop: '1em'
+	}
+};
 
 const INITIAL_STATE = {
 	email: '',
@@ -28,7 +39,7 @@ class SignIn extends Component {
 
 		firebase
 			.doSignInWithEmailAndPassword(email, password)
-			.then(authUser => {
+			.then(() => {
 				this.setState({ ...INITIAL_STATE });
 				history.push('/');
 			})
@@ -44,7 +55,7 @@ class SignIn extends Component {
 		const { firebase, history } = this.props;
 		firebase
 			.doSignInWithGoogle()
-			.then(authUser => {
+			.then(() => {
 				this.setState({ ...INITIAL_STATE });
 				history.push('/');
 			})
@@ -59,48 +70,80 @@ class SignIn extends Component {
 	}
 
 	render() {
+		const { classes } = this.props;
 		const { error } = this.state;
 		return (
 			<>
+				<div style={{ flexGrow: 1, textAlign: 'center' }}>
+					<FacebookLoginButton
+						align='center'
+						onClick={() => alert("Sorry Facebook isn't supported yet")}
+					></FacebookLoginButton>
+					<TwitterLoginButton
+						align='center'
+						onClick={() => alert("Sorry Twitter isn't supported yet")}
+					/>
+					<GoogleLoginButton
+						align='center'
+						onClick={e => this.onSubmitGoogle(e)}
+					></GoogleLoginButton>
+				</div>
+
+				<br />
+				<hr />
+				<br />
+
 				<form onSubmit={e => this.onSubmitEmail(e)}>
 					<Grid container spacing={2}>
 						<Grid item xs={12}>
 							<TextField
+								autoComplete
+								autoFocus
+								fullWidth
 								variant='outlined'
 								name='email'
-								helperText='Email'
+								placeholder='Email'
 								onChange={e => this.handleInputChange(e)}
 							/>
 						</Grid>
 						<Grid item xs={12}>
 							<TextField
+								autoComplete
+								fullWidth
 								variant='outlined'
 								type='password'
 								name='password'
-								helperText='Password'
+								placeholder='Password'
 								onChange={e => this.handleInputChange(e)}
 							/>
 						</Grid>
-						<Grid item>
+						<Grid item xs={12}>
 							<Button
+								fullWidth
 								disabled={this.isInvalid()}
 								type='submit'
 								variant='contained'
 								color='primary'
 							>
-								Sign In
+								Login
 							</Button>
 						</Grid>
 					</Grid>
 					{error && <p>{error.message}</p>}
 				</form>
-				<Button onClick={e => this.onSubmitGoogle(e)}>Sign In With Google</Button>
-				<Typography variant='body2'>
-					New to mycookbook? <Link to='/signup'>SIGN UP</Link>
-				</Typography>
+				<br />
+				<div className={classes.signUp}>
+					<Typography variant='body2'>
+						New to mycookbook?{' '}
+						<b>
+							<Link to='/signup'>Create an account</Link>
+						</b>
+						.
+					</Typography>
+				</div>
 			</>
 		);
 	}
 }
 
-export default withRouter(withFirebase(SignIn));
+export default withStyles(styles)(withRouter(withFirebase(SignIn)));
