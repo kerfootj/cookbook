0c8260b1149f5c6a4505c8ebb82a9212b2f447e6
Joel Kerfoot <joelkerfoot@gmail.com>
2019-10-10 20:42:45 -0700

    added an image gallery, closes #32

--- src/Organisms/Gallery.jsx
+++ src/Organisms/Gallery.jsx
@@ -1,3 +1,5 @@
+import 'react-image-gallery/styles/scss/image-gallery.scss';
+
 import React, { Component } from 'react';
 
 import ImageGallery from 'react-image-gallery';
@@ -6,11 +8,7 @@ class Gallery extends Component {
 	render() {
 		const { items } = this.props;
 		return (
-			<ImageGallery
-				items={items}
-				renderPlayPauseButton={() => {}}
-				renderFullscreenButton={() => {}}
-			/>
+			<ImageGallery items={items} showPlayButton={false} renderFullscreenButton={() => {}} />
 		);
 	}
 }
