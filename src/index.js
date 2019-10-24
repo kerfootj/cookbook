89fd00ca6e7826046e6252387d04e2885ba5155e
Joel Kerfoot <joelkerfoot@gmail.com>
2019-10-23 21:46:16 -0700

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import * as serviceWorker from './serviceWorker';
 
-import Firebase, {
-	FirebaseContext
-} from './Atoms/Firebase/index';
+import Firebase, { FirebaseContext } from './Atoms/Firebase/index';
 
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( <
-	FirebaseContext.Provider value = {
-		new Firebase()
-	} >
-	<
-	App / >
-	<
-	/FirebaseContext.Provider>,
-	document.getElementById('root')
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
 );
 
 serviceWorker.unregister();
