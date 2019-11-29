380307364b9b096d084429d33f55c150c33fd958
Joel Kerfoot <joelkerfoot@gmail.com>
2019-11-29 12:19:05 -0800

    added email and password textfields

---
+++ src/Atoms/textfields/defaultTypes.js
@@ -0,0 +1,22 @@
+const NumberDefaultProps = {
+  defaultValue: undefined,
+  name: undefined,
+  max: undefined,
+  min: undefined,
+};
+
+const RecipeDefaultProps = {
+  defaultValue: undefined,
+  name: undefined,
+  placeholder: undefined,
+  rows: undefined,
+};
+
+const TimeDefaultProps = {
+  defaultValue: undefined,
+  name: undefined,
+  placeholder: undefined,
+  valid: true,
+};
+
+export { NumberDefaultProps, RecipeDefaultProps, TimeDefaultProps };
