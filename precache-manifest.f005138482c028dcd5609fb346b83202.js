4b5a55b8407467cd570dd454b7138cffb6358b3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-05 22:34:44 +0000

    Deploy kerfootj/cookbook to github.com/kerfootj/cookbook.git:gh-pages

---
+++ precache-manifest.f005138482c028dcd5609fb346b83202.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f8730d4464dc1364bcfe5a68a3445f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5446103b1326b5f9228",
+    "url": "/static/css/2.abe13ccb.chunk.css"
+  },
+  {
+    "revision": "f9523cac50103995f19c",
+    "url": "/static/css/main.4e5b5ff3.chunk.css"
+  },
+  {
+    "revision": "f5446103b1326b5f9228",
+    "url": "/static/js/2.6a4f8af1.chunk.js"
+  },
+  {
+    "revision": "c3ce0d5fa81bfac1eee42a8695a9a6b5",
+    "url": "/static/js/2.6a4f8af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9523cac50103995f19c",
+    "url": "/static/js/main.33524ae9.chunk.js"
+  },
+  {
+    "revision": "f01a219499ca9fedc5fa",
+    "url": "/static/js/runtime-main.65a4e054.js"
+  },
+  {
+    "revision": "4dbbd9f79a83d966931c75ea61247265",
+    "url": "/static/media/burger.4dbbd9f7.gif"
+  },
+  {
+    "revision": "a7e354cedb774d0cd18ec579009e673b",
+    "url": "/static/media/coffee.a7e354ce.jpg"
+  },
+  {
+    "revision": "ce3f22a7243968ca118dec80b9c938bb",
+    "url": "/static/media/logo.ce3f22a7.png"
+  }
+]);
